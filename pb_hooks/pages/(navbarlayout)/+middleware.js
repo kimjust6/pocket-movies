4f7e85be3b0d04dc8ff8af7954f2e7f9a0adfb53
Justin Kim <mletemps@hotmail.com>
2026-01-17 15:47:17 -0500

    fixed ogtag image dimensions

--- pb_hooks/pages/(navbarlayout)/+middleware.js
+++ pb_hooks/pages/(navbarlayout)/+middleware.js
@@ -28,8 +28,8 @@ module.exports = function (api) {
                 content: 'https://ylam.jkim.win/og-image.webp',
             },
             { name: 'og:image:alt', content: 'Yvonne and Justin Wedding Photo' },
-            { name: 'og:image:width', content: '1200' },
-            { name: 'og:image:height', content: '630' },
+            { name: 'og:image:width', content: '637' },
+            { name: 'og:image:height', content: '425' },
             {
                 name: 'og:description',
                 content:
